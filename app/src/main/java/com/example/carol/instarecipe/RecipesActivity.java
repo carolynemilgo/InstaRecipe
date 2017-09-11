db57909a63658bffff30546d2722b40855f3042c
carolynemilgo <carolynemilgo@gmail.com>
2017-09-11 13:10:08 +0300

    updated layout files for recipe activities

--- app/src/main/java/com/example/carol/instarecipe/RecipesActivity.java
+++ app/src/main/java/com/example/carol/instarecipe/RecipesActivity.java
@@ -3,10 +3,14 @@ package com.example.carol.instarecipe;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.TextView;
 
 public class RecipesActivity extends AppCompatActivity {
     private TextView mRecipeTextView;
+    private RecyclerView mRecyclerView;
+    private LinearLayoutManager mLinearLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,11 @@ public class RecipesActivity extends AppCompatActivity {
         Intent findRecipes=getIntent();
         String guestName=findRecipes.getStringExtra("guestName");
         mRecipeTextView.setText("Hello " +guestName+".We invite you to savour our delicious recipes!");
+
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        mLinearLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLinearLayoutManager);
     }
 }
 
