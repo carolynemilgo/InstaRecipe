da7fd070c42c6bf6aa89806575edd6ec80d3f6fe
carolynemilgo <carolynemilgo@gmail.com>
2017-09-22 15:20:31 +0300

    Updated SavedRecipeListACtivity to allow viewing of data pulled from firebase

---
+++ app/src/main/java/com/example/carol/instarecipe/SavedRecipeListActivity.java
@@ -0,0 +1,12 @@
+package com.example.carol.instarecipe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SavedRecipeListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
