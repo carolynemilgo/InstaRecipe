832311c2c54ced6a6a8958d30f0b5391632020ff
carolynemilgo <carolynemilgo@gmail.com>
2017-09-28 12:43:03 +0300

    updated the onCreateOptionsMenu method for inflating the overflow menu within the main activity

--- app/src/main/java/com/example/carol/instarecipe/MainActivity.java
+++ app/src/main/java/com/example/carol/instarecipe/MainActivity.java
@@ -5,6 +5,8 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -140,6 +142,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mSearchedRecipeReference.setValue(recipes);
     }
 
+    //used to inflate the overflow menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
 
 
