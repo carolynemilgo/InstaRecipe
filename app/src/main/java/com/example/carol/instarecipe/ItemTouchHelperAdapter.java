2cf714370ceb2a105ea48d9aa5cca06fb8a9800f
carolynemilgo <carolynemilgo@gmail.com>
2017-09-29 15:05:49 +0300

    updated 2 interfaces for the itemTouchHelper

---
+++ app/src/main/java/com/example/carol/instarecipe/ItemTouchHelperAdapter.java
@@ -0,0 +1,10 @@
+package com.example.carol.instarecipe;
+
+/**
+ * Created by carol on 9/29/17.
+ */
+
+public class ItemTouchHelperAdapter {
+    boolean onItemMove(int fromPosition, int toPosition);
+    void onItemDismiss(int position);
+}
---
+++ app/src/main/java/com/example/carol/instarecipe/OnStartDragListener.java
@@ -0,0 +1,11 @@
+package com.example.carol.instarecipe;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by carol on 9/29/17.
+ */
+
+public class OnStartDragListener {
+    void onStartDrag(RecyclerView.ViewHolder viewHolder);
+}
